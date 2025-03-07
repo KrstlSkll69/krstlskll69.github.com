8089568897752de7535f69e8a42a07de69e2e21f
Krystal <150982280+KrstlSkll69@users.noreply.github.com>
2025-03-07 17:45:15 -0500

    test

--- Javascript/OneKo.js
+++ Javascript/OneKo.js
@@ -1,138 +1,265 @@
 // oneko.js ~~ https://github.com/adryd325/oneko.js
 // Pet the cat version ~~ https://github.com/tylxr59/oneko.js/tree/main
 
-(function oneko() {
-  const isReducedMotion =
-    window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
-    window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
-
-  if (isReducedMotion) return;
-
+(async function oneko() {
   const nekoEl = document.createElement("div");
+  let nekoPosX = 32,
+    nekoPosY = 32,
+    mousePosX = 0,
+    mousePosY = 0,
+    frameCount = 0,
+    idleTime = 0,
+    idleAnimation = null,
+    idleAnimationFrame = 0,
+    forceSleep = false,
+    grabbing = false,
+    grabStop = true,
+    nudge = false,
+    kuroNeko = false,
+    variant = "classic";
 
-  let nekoPosX = 32;
-  let nekoPosY = 32;
+  function parseLocalStorage(key, fallback) {
+    try {
+      const value = JSON.parse(localStorage.getItem(`oneko:${key}`));
+      return typeof value === typeof fallback ? value : fallback;
+    } catch (e) {
+      console.error(e);
+      return fallback;
+    }
+  }
 
-  let mousePosX = 0;
-  let mousePosY = 0;
+  const nekoSpeed = 10,
+    variants = [
+      ["classic", "Classic"],
+      ["dog", "Dog"],
+      ["tora", "Tora"],
+      ["maia", "Maia (maia.crimew.gay)"],
+      ["vaporwave", "Vaporwave (nya.rest)"],
+    ],
+    spriteSets = {
+      idle: [[-3, -3]],
+      alert: [[-7, -3]],
+      scratchSelf: [
+        [-5, 0],
+        [-6, 0],
+        [-7, 0],
+      ],
+      scratchWallN: [
+        [0, 0],
+        [0, -1],
+      ],
+      scratchWallS: [
+        [-7, -1],
+        [-6, -2],
+      ],
+      scratchWallE: [
+        [-2, -2],
+        [-2, -3],
+      ],
+      scratchWallW: [
+        [-4, 0],
+        [-4, -1],
+      ],
+      tired: [[-3, -2]],
+      sleeping: [
+        [-2, 0],
+        [-2, -1],
+      ],
+      N: [
+        [-1, -2],
+        [-1, -3],
+      ],
+      NE: [
+        [0, -2],
+        [0, -3],
+      ],
+      E: [
+        [-3, 0],
+        [-3, -1],
+      ],
+      SE: [
+        [-5, -1],
+        [-5, -2],
+      ],
+      S: [
+        [-6, -3],
+        [-7, -2],
+      ],
+      SW: [
+        [-5, -3],
+        [-6, -1],
+      ],
+      W: [
+        [-4, -2],
+        [-4, -3],
+      ],
+      NW: [
+        [-1, 0],
+        [-1, -1],
+      ],
+    }, // Get keys with 2 or more sprites
+    keys = Object.keys(spriteSets).filter((key) => spriteSets[key].length > 1),
+    usedKeys = new Set();
 
-  let frameCount = 0;
-  let idleTime = 0;
-  let idleAnimation = null;
-  let idleAnimationFrame = 0;
+  function sleep() {
+    forceSleep = !forceSleep;
+    nudge = false;
+    localStorage.setItem("oneko:forceSleep", forceSleep);
+    if (!forceSleep) {
+      resetIdleAnimation();
+      return;
+    }
 
-  const nekoSpeed = 10;
-  const spriteSets = {
-    idle: [[-3, -3]],
-    alert: [[-7, -3]],
-    scratchSelf: [
-      [-5, 0],
-      [-6, 0],
-      [-7, 0],
-    ],
-    scratchWallN: [
-      [0, 0],
-      [0, -1],
-    ],
-    scratchWallS: [
-      [-7, -1],
-      [-6, -2],
-    ],
-    scratchWallE: [
-      [-2, -2],
-      [-2, -3],
-    ],
-    scratchWallW: [
-      [-4, 0],
-      [-4, -1],
-    ],
-    tired: [[-3, -2]],
-    sleeping: [
-      [-2, 0],
-      [-2, -1],
-    ],
-    N: [
-      [-1, -2],
-      [-1, -3],
-    ],
-    NE: [
-      [0, -2],
-      [0, -3],
-    ],
-    E: [
-      [-3, 0],
-      [-3, -1],
-    ],
-    SE: [
-      [-5, -1],
-      [-5, -2],
-    ],
-    S: [
-      [-6, -3],
-      [-7, -2],
-    ],
-    SW: [
-      [-5, -3],
-      [-6, -1],
-    ],
-    W: [
-      [-4, -2],
-      [-4, -3],
-    ],
-    NW: [
-      [-1, 0],
-      [-1, -1],
-    ],
-  };
+    // If Full App Display is on, sleep on its progress bar instead
+    const fullAppDisplay = document.getElementById("fad-progress");
+    if (fullAppDisplay) {
+      mousePosX = fullAppDisplay.getBoundingClientRect().right - 16;
+      mousePosY = fullAppDisplay.getBoundingClientRect().top - 12;
+      return;
+    }
 
-  function init() {
+    // Get the far right and top of the progress bar
+    const progressBar = document.querySelector(".main-nowPlayingBar-center .playback-progressbar");
+    const progressBarRight = progressBar.getBoundingClientRect().right;
+    const progressBarTop = progressBar.getBoundingClientRect().top;
+    const progressBarBottom = progressBar.getBoundingClientRect().bottom;
+
+    // Make the cat sleep on the progress bar
+    mousePosX = progressBarRight - 16;
+    mousePosY = progressBarTop - 8;
+
+    // Get the position of the remaining time
+    const remainingTime = document.querySelector(".main-playbackBarRemainingTime-container");
+    const remainingTimeLeft = remainingTime.getBoundingClientRect().left;
+    const remainingTimeBottom = remainingTime.getBoundingClientRect().bottom;
+    const remainingTimeTop = remainingTime.getBoundingClientRect().top;
+
+    // Get the position of elapsed time
+    const elapsedTime = document.querySelector(".playback-bar__progress-time-elapsed");
+    const elapsedTimeRight = elapsedTime.getBoundingClientRect().right;
+    const elapsedTimeLeft = elapsedTime.getBoundingClientRect().left;
+
+    // If the remaining time is on top right of the progress bar, make the cat sleep to the a little bit to the left of the remaining time
+    // Theme compatibility
+    if (remainingTimeLeft < progressBarRight && remainingTimeTop < progressBarBottom && progressBarTop - remainingTimeBottom < 32) {
+      mousePosX = remainingTimeLeft - 16;
+
+      // Move the cat to the left of elapsed time if it is too close to the remaining time (Nord theme)
+      if (remainingTimeLeft - elapsedTimeRight < 32) {
+        mousePosX = elapsedTimeLeft - 16;
+      }
+    }
+  }
+
+  function create() {
+    variant = parseLocalStorage("variant", "classic");
+    const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    kuroNeko = prefersDarkMode ? false : true;
+    localStorage.setItem("oneko:kuroneko", JSON.stringify(kuroNeko));
+
+    if (!variants.some((v) => v[0] === variant)) {
+      variant = "classic";
+    }
+
     nekoEl.id = "oneko";
-    nekoEl.ariaHidden = true;
     nekoEl.style.width = "32px";
     nekoEl.style.height = "32px";
     nekoEl.style.position = "fixed";
-    nekoEl.style.pointerEvents = "auto";
+    // nekoEl.style.pointerEvents = "none";
+    nekoEl.style.backgroundImage = `url('https://cdn.nest.rip/uploads/e17c87d3-4f9a-4eb1-b49b-c70144f6910d.png')`;
     nekoEl.style.imageRendering = "pixelated";
     nekoEl.style.left = `${nekoPosX - 16}px`;
     nekoEl.style.top = `${nekoPosY - 16}px`;
-    nekoEl.style.zIndex = 2147483647;
-
-    let nekoFile = "https://cdn.nest.rip/uploads/e17c87d3-4f9a-4eb1-b49b-c70144f6910d.png"
-    const curScript = document.currentScript
-    if (curScript && curScript.dataset.cat) {
-      nekoFile = curScript.dataset.cat
-    }
-    nekoEl.style.backgroundImage = `url(${nekoFile})`;
+    nekoEl.style.filter = kuroNeko ? "invert(100%)" : "none";
 
     document.body.appendChild(nekoEl);
 
-    document.addEventListener("mousemove", function (event) {
-      mousePosX = event.clientX;
-      mousePosY = event.clientY;
+    window.addEventListener("mousemove", (e) => {
+      if (forceSleep) return;
+
+      mousePosX = e.clientX;
+      mousePosY = e.clientY;
     });
 
-    window.requestAnimationFrame(onAnimationFrame);
+    window.addEventListener("resize", () => {
+      if (forceSleep) {
+        forceSleep = false;
+        sleep();
+      }
+    });
+
+    // Handle dragging of the cat
+    nekoEl.addEventListener("mousedown", (e) => {
+      if (e.button !== 0) return;
+      grabbing = true;
+      let startX = e.clientX;
+      let startY = e.clientY;
+      let startNekoX = nekoPosX;
+      let startNekoY = nekoPosY;
+      let grabInterval;
+
+      const mousemove = (e) => {
+        const deltaX = e.clientX - startX;
+        const deltaY = e.clientY - startY;
+        const absDeltaX = Math.abs(deltaX);
+        const absDeltaY = Math.abs(deltaY);
+
+        // Scratch in the opposite direction of the drag
+        if (absDeltaX > absDeltaY && absDeltaX > 10) {
+          setSprite(deltaX > 0 ? "scratchWallW" : "scratchWallE", frameCount);
+        } else if (absDeltaY > absDeltaX && absDeltaY > 10) {
+          setSprite(deltaY > 0 ? "scratchWallN" : "scratchWallS", frameCount);
+        }
+
+        if (grabStop || absDeltaX > 10 || absDeltaY > 10 || Math.sqrt(deltaX ** 2 + deltaY ** 2) > 10) {
+          grabStop = false;
+          clearTimeout(grabInterval);
+          grabInterval = setTimeout(() => {
+            grabStop = true;
+            nudge = false;
+            startX = e.clientX;
+            startY = e.clientY;
+            startNekoX = nekoPosX;
+            startNekoY = nekoPosY;
+          }, 150);
+        }
+
+        nekoPosX = startNekoX + e.clientX - startX;
+        nekoPosY = startNekoY + e.clientY - startY;
+        nekoEl.style.left = `${nekoPosX - 16}px`;
+        nekoEl.style.top = `${nekoPosY - 16}px`;
+      };
+
+      const mouseup = () => {
+        grabbing = false;
+        nudge = true;
+        resetIdleAnimation();
+        window.removeEventListener("mousemove", mousemove);
+        window.removeEventListener("mouseup", mouseup);
+      };
+
+      window.addEventListener("mousemove", mousemove);
+      window.addEventListener("mouseup", mouseup);
+    });
+
+    nekoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      kuroNeko = !kuroNeko;
+      localStorage.setItem("oneko:kuroneko", kuroNeko);
+      nekoEl.style.filter = kuroNeko ? "invert(100%)" : "none";
+    });
+
+    nekoEl.addEventListener("dblclick", sleep);
+
+    window.onekoInterval = setInterval(frame, 100);
   }
 
-  let lastFrameTimestamp;
-
-  function onAnimationFrame(timestamp) {
-    // Stops execution if the neko element is removed from DOM
-    if (!nekoEl.isConnected) {
-      return;
-    }
-    if (!lastFrameTimestamp) {
-      lastFrameTimestamp = timestamp;
-    }
-    if (timestamp - lastFrameTimestamp > 100) {
-      lastFrameTimestamp = timestamp
-      frame()
-    }
-    window.requestAnimationFrame(onAnimationFrame);
+  function getSprite(name, frame) {
+    return spriteSets[name][frame % spriteSets[name].length];
   }
 
   function setSprite(name, frame) {
-    const sprite = spriteSets[name][frame % spriteSets[name].length];
+    const sprite = getSprite(name, frame);
     nekoEl.style.backgroundPosition = `${sprite[0] * 32}px ${sprite[1] * 32}px`;
   }
 
@@ -145,38 +272,43 @@
     idleTime += 1;
 
     // every ~ 20 seconds
-    if (
-      idleTime > 10 &&
-      Math.floor(Math.random() * 200) == 0 &&
-      idleAnimation == null
-    ) {
-      let availableIdleAnimations = ["sleeping", "scratchSelf"];
+    if (idleTime > 10 && Math.floor(Math.random() * 200) == 0 && idleAnimation == null) {
+      let avalibleIdleAnimations = ["sleeping", "scratchSelf"];
       if (nekoPosX < 32) {
-        availableIdleAnimations.push("scratchWallW");
+        avalibleIdleAnimations.push("scratchWallW");
       }
       if (nekoPosY < 32) {
-        availableIdleAnimations.push("scratchWallN");
+        avalibleIdleAnimations.push("scratchWallN");
       }
       if (nekoPosX > window.innerWidth - 32) {
-        availableIdleAnimations.push("scratchWallE");
+        avalibleIdleAnimations.push("scratchWallE");
       }
       if (nekoPosY > window.innerHeight - 32) {
-        availableIdleAnimations.push("scratchWallS");
+        avalibleIdleAnimations.push("scratchWallS");
       }
-      idleAnimation =
-        availableIdleAnimations[
-        Math.floor(Math.random() * availableIdleAnimations.length)
-        ];
+      idleAnimation = avalibleIdleAnimations[Math.floor(Math.random() * avalibleIdleAnimations.length)];
+    }
+
+    if (forceSleep) {
+      avalibleIdleAnimations = ["sleeping"];
+      idleAnimation = "sleeping";
     }
 
     switch (idleAnimation) {
       case "sleeping":
+        if (idleAnimationFrame < 8 && nudge && forceSleep) {
+          setSprite("idle", 0);
+          break;
+        } else if (nudge) {
+          nudge = false;
+          resetIdleAnimation();
+        }
         if (idleAnimationFrame < 8) {
           setSprite("tired", 0);
           break;
         }
         setSprite("sleeping", Math.floor(idleAnimationFrame / 4));
-        if (idleAnimationFrame > 192) {
+        if (idleAnimationFrame > 192 && !forceSleep) {
           resetIdleAnimation();
         }
         break;
@@ -197,57 +329,31 @@
     idleAnimationFrame += 1;
   }
 
-  function explodeHearts() {
-    const parent = nekoEl.parentElement;
-    const rect = nekoEl.getBoundingClientRect();
-    const scrollLeft = window.scrollX || document.documentElement.scrollLeft;
-    const scrollTop = window.scrollY || document.documentElement.scrollTop;
-    const centerX = rect.left + rect.width / 2 + scrollLeft;
-    const centerY = rect.top + rect.height / 2 + scrollTop;
-
-    for (let i = 0; i < 10; i++) {
-      const heart = document.createElement('div');
-      heart.className = 'heart';
-      heart.textContent = '❤';
-      const offsetX = (Math.random() - 0.5) * 50;
-      const offsetY = (Math.random() - 0.5) * 50;
-      heart.style.left = `${centerX + offsetX - 16}px`;
-      heart.style.top = `${centerY + offsetY - 16}px`;
-      heart.style.transform = `translate(-50%, -50%) rotate(${Math.random() * 360}deg)`;
-      parent.appendChild(heart);
-
-      setTimeout(() => {
-        parent.removeChild(heart);
-      }, 1000);
-    }
-  }
-
-  const style = document.createElement('style');
-  style.innerHTML = `
-		  @keyframes heartBurst {
-			  0% { transform: scale(0); opacity: 1; }
-			  100% { transform: scale(1); opacity: 0; }
-		  }
-		  .heart {
-			  position: absolute;
-			  font-size: 2em;
-			  animation: heartBurst 1s ease-out;
-			  animation-fill-mode: forwards;
-			  color:  var(--OnekoHearts);
-        filter: var(--OnekoHearts-filter);
-		  }
-	  `;
-
-  document.head.appendChild(style);
-  nekoEl.addEventListener('click', explodeHearts);
-
   function frame() {
     frameCount += 1;
+
+    if (grabbing) {
+      grabStop && setSprite("alert", 0);
+      return;
+    }
+
     const diffX = nekoPosX - mousePosX;
     const diffY = nekoPosY - mousePosY;
     const distance = Math.sqrt(diffX ** 2 + diffY ** 2);
 
-    if (distance < nekoSpeed || distance < 48) {
+    // Cat has to sleep on top of the progress bar
+    if (forceSleep && Math.abs(diffY) < nekoSpeed && Math.abs(diffX) < nekoSpeed) {
+      // Make the cat sleep exactly on the top of the progress bar
+      nekoPosX = mousePosX;
+      nekoPosY = mousePosY;
+      nekoEl.style.left = `${nekoPosX - 16}px`;
+      nekoEl.style.top = `${nekoPosY - 16}px`;
+
+      idle();
+      return;
+    }
+
+    if ((distance < nekoSpeed || distance < 48) && !forceSleep) {
       idle();
       return;
     }
@@ -263,7 +369,6 @@
       return;
     }
 
-    let direction;
     direction = diffY / distance > 0.5 ? "N" : "";
     direction += diffY / distance < -0.5 ? "S" : "";
     direction += diffX / distance > 0.5 ? "W" : "";
@@ -280,6 +385,99 @@
     nekoEl.style.top = `${nekoPosY - 16}px`;
   }
 
-  init();
-    console.log('OneKo successfully loaded');
+  create();
+
+  function getRandomSprite() {
+    let unusedKeys = keys.filter((key) => !usedKeys.has(key));
+    if (unusedKeys.length === 0) {
+      usedKeys.clear();
+      unusedKeys = keys;
+    }
+    const index = Math.floor(Math.random() * unusedKeys.length);
+    const key = unusedKeys[index];
+    usedKeys.add(key);
+    return [getSprite(key, 0), getSprite(key, 1)];
+  }
+
+  function setVariant(arr) {
+    console.log(arr);
+
+    variant = arr[0];
+    localStorage.setItem("oneko:variant", `"${variant}"`);
+    nekoEl.style.backgroundImage = `url('https://i.chenna.me/oneko/oneko-${variant}.gif')`;
+  }
+
+  // Popup modal to choose variant
+  function pickerModal() {
+    const container = document.createElement("div");
+    container.className = "oneko-variant-container";
+
+    const style = document.createElement("style");
+    // Each variant is a 64x64 sprite
+    style.innerHTML = `
+      .oneko-variant-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+      }
+      .oneko-variant-button {
+        width: 64px;
+        height: 64px;
+        margin: 8px;
+        cursor: pointer;
+        background-size: 800%;
+        border-radius: 25%;
+        transition: background-color 0.2s ease-in-out;
+        background-position: var(--idle-x) var(--idle-y);
+        image-rendering: pixelated;
+      }
+      .oneko-variant-button:hover, .oneko-variant-button-selected {
+        background-color: var(--spice-main-elevated);
+      }
+      .oneko-variant-button:hover {
+        background-position: var(--active-x) var(--active-y);
+      }
+    `;
+    container.appendChild(style);
+
+    const [idle, active] = getRandomSprite();
+
+    function variantButton(variantEnum) {
+      const div = document.createElement("div");
+
+      div.className = "oneko-variant-button";
+      div.id = variantEnum[0];
+      div.style.backgroundImage = `url('https://i.chenna.me/oneko/oneko-${variantEnum[0]}.gif')`;
+      div.style.setProperty("--idle-x", `${idle[0] * 64}px`);
+      div.style.setProperty("--idle-y", `${idle[1] * 64}px`);
+      div.style.setProperty("--active-x", `${active[0] * 64}px`);
+      div.style.setProperty("--active-y", `${active[1] * 64}px`);
+
+      div.onclick = () => {
+        setVariant(variantEnum);
+        document.querySelector(".oneko-variant-button-selected")?.classList.remove("oneko-variant-button-selected");
+        div.classList.add("oneko-variant-button-selected");
+      };
+
+      if (variantEnum[0] === variant) {
+        div.classList.add("oneko-variant-button-selected");
+      }
+
+      return div;
+    }
+
+    for (const variant of variants) {
+      container.appendChild(variantButton(variant));
+    }
+
+    return container;
+  }
+
+  if (parseLocalStorage("forceSleep", false)) {
+    while (!document.querySelector(".main-nowPlayingBar-center .playback-progressbar")) {
+      await new Promise((r) => setTimeout(r, 100));
+    }
+    sleep();
+  }
 })();
