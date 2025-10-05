ab71d270bfb01941f3ad93189106840bd5d0514b
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-10-05 12:07:54 -0400

    bleh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
-import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://indi.is-a-skid.cc',
-    output: 'server',
+    output: 'static',
     vite: {
         plugins: [tailwindcss()],
     },
-    adapter: node({
-        mode: 'standalone',
-    }),
 })
