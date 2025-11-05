d863a8e71df944520c71809ca21c5eab64fdaecd
Indiana Jone (Indi) <150982280+KrstlSkll69@users.noreply.github.com>
2025-11-04 20:48:42 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://krstlskll69.github.io',
+    site: 'https://krystal.thororen.com/',
     output: 'static',
     vite: {
         plugins: [tailwindcss()],
